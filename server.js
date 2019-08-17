6061f715140304479139d8a7b997aea487a32b6e
cmlee <bal.dongdongdong@gmail.com>
2019-08-17 20:48:17 +0900

    update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const { createServer } = require('http');
 const { parse } = require('url');
 const next = require('next');
 
+const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
@@ -12,8 +13,8 @@ app.prepare().then(() => {
     const { pathname, query } = parsedUrl;
 
     handle(req, res, parsedUrl);
-  }).listen(process.env.PORT || 3000, err => {
+  }).listen(port, err => {
     if (err) throw err;
-    console.log('> Ready on http://localhost:3000')
-  });
+    console.log(`> Ready on http://localhost:${port}`)
+  })
 });
