745fa6500370c795967de3207cdb3c9ceb6264da
cmlee <bal.dongdongdong@gmail.com>
2019-09-02 19:48:21 +0900

    add deploy

---
+++ ecosystem.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  apps: [{
+    name: 'gns-client',
+    script: './server.js',
+
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '512M',
+    env: {
+      NODE_ENV: 'development',
+    },
+    env_production: {
+      NODE_ENV: 'production',
+    },
+  }],
+  deploy: {
+    production: {
+      user: 'ec2-user',
+      key: '/Users/cmlee/.ssh/LightsailDefaultPrivateKey-ap-northeast-2.pem',
+      host: '13.209.56.51',
+      ref: 'origin/master',
+      repo: 'https://github.com/almond-bongbong/gns_client.git',
+      path: '/deploy/gns-client',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
+    },
+  },
+};
